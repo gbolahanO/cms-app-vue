d70b76c595e3d16a43ceaab834bb93a3ead21cce
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-07-14 09:45:57 +0100

    made auth package accessible globally

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import auth from './packages/auth/Auth'
 
 Vue.config.productionTip = false
+Vue.use(auth)
 
 /* eslint-disable no-new */
 new Vue({
