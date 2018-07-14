22452cfcdbeae425b7605bd0ea3418ff927917a9
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-07-14 09:41:26 +0100

    added comments to the functions describing their roles

--- src/packages/auth/Auth.js
+++ src/packages/auth/Auth.js
@@ -1,9 +1,14 @@
 export default function (Vue) {
   Vue.auth = {
+
+    // Set token and expiration returned from api
+
     setToken: (token, expiresIn) => {
       localStorage.setItem('token', token);
       localStorage.setItem('expiresIn', expiresIn);
     },
+
+    // Get token stored in localStorage, return null if there is no token
 
     getToken: () => {
       let token = localStorage.getItem('token');
@@ -20,10 +25,15 @@ export default function (Vue) {
       }
     },
 
+    // Remove token from localStorage
+    
     destroyToken: () => {
       localStorage.removeItem('token');
       localStorage.removeItem('expiresIn');
     },
+
+    // Check if a token was stored when a user logs in, if there is none return false
+    // and don't authenticate the user
 
     isAuthenticated: () => {
       if (this.getToken())
@@ -32,6 +42,10 @@ export default function (Vue) {
         return false;
     }
   }
+
+  // Create $auth property which returns the functions above
+  // so we can access it as this.$auth.* to call the function to be used
+  // e.g this.$auth.setToken()
 
   Object.defineProperties(Vue.prototype, {
     $auth: {
