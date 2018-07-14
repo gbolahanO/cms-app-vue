693c5a0ac492afca81a2d7327ea742488c44f15d
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-07-14 08:13:46 +0100

    added route link to login in navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -31,6 +31,11 @@
           <li class="nav-item">
             <a class="nav-link" href="#">About me</a>
           </li>
+          <li class="nav-item">
+            <router-link to="/login">
+              <a class="nav-link">Login</a>
+            </router-link>
+          </li>
         </ul>
         <form class="form-inline mt-2 mt-md-0">
           <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
