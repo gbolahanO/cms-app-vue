1ef05f1b99e3005dfb66362f7cab99f1ed4c77e6
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-07-14 08:23:45 +0100

    created login component

---
+++ src/components/authentication/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 m-auto">
+        <div class="card mt-5">
+          <div class="card-body">
+            <form @submit.prevent="login" class="form-signin">
+              <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+              
+              <label class="sr-only mb-2">Email address</label>
+              <input v-model="email" type="email" class="form-control" placeholder="Email address" >
+
+              <label class="sr-only">Password</label>
+              <input v-model="password" type="password" class="form-control" placeholder="Password" >
+
+              <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+              <p class="mt-5 mb-3 text-muted">&copy; 2018</p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      console.log(`${this.email} ${this.password}`);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
