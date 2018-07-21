c6ac1a2d653f8854b595ec59a860e08f7fbb0647
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-07-21 15:13:11 +0100

    forgot closing div tag

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/authentication/Login'
 import Dashboard from '@/components/Dashboard'
+import Posts from '@/components/Posts'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/dashboard',
       name: 'Dashboard',
       component: Dashboard
+    },
+    {
+      path: '/posts',
+      name: 'Posts',
+      component: Posts
     }
   ]
 })
