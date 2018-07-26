4ec33ad18fb818dec0095f72ac12da2728cf8407
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-07-26 21:15:50 +0100

    refactored design

--- src/components/authentication/Login.vue
+++ src/components/authentication/Login.vue
@@ -5,16 +5,14 @@
         <div class="card mt-5">
           <div class="card-body">
             <form @submit.prevent="login" class="form-signin">
-              <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+              <h1 class="h3 mb-4 font-weight-normal">Please sign in</h1>
 
-              <label class="sr-only mb-2">Email address</label>
-              <input v-model="email" type="email" class="form-control" placeholder="Email address" >
+              <input v-model="email" type="email" class="form-control mb-3" placeholder="Email address" >
 
-              <label class="sr-only">Password</label>
-              <input v-model="password" type="password" class="form-control" placeholder="Password" >
+              <input v-model="password" type="password" class="form-control mb-4" placeholder="Password" >
 
               <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-              <p class="mt-5 mb-3 text-muted">&copy; 2018</p>
+              <p class="mt-4 mb-1 text-muted">&copy; 2018</p>
             </form>
           </div>
         </div>
