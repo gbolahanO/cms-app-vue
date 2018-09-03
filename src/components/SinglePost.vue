eca5053a3d4ce0dc195c3e1cc2e5c7f600137fb2
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-09-03 19:14:50 +0100

    setup single post layout and code

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,13 +1,62 @@
 <template>
-  
+  <div>
+    <div class="container-fluid px-4 mt-5">
+      <div class="row mb-5">
+        <div class="col-lg-7 text-center m-auto">
+          <span class="">
+            {{ post.created_at}} / {{ category }}
+          </span>
+          <h1 class="display-5 text-center">
+            {{ post.title }}
+          </h1>
+        </div>
+      </div>
+      <div class="row px-0 mb-5">
+        <div class="col-lg-12 px-0">
+          <img class="rounded w-100" :src="post.post_image" alt="">
+        </div>
+        <div class="col-lg-10 m-auto">
+          <div class="card border-0 px-5 negative-margin">
+            <div class="card-body">
+              {{ post.content }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      slug: this.$route.params.slug,
+      post: {},
+      category: ""
+    }
+  },
+  created() {
+    this.getSinglePost()
+  },
+  methods: {
+    getSinglePost: function() {
+      this.$http.get('api/blog/' + this.slug)
+        .then((response) => {
+          console.log(response);
+          this.post = response.body.post;
+          this.category = response.body.category;
+        })
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.negative-margin {
+  margin-top: -100px;
+  font-size: 1.6rem;
+  line-height: 1.5;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.24)
+}
 </style>
